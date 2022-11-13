44cbee09a6fb7ac497bc05fb977ec594be7a19f1
Brian <briangordon.cahill@students.dominican.edu>
2022-11-12 23:05:36 -0800

    Fixed attacking bug, added comments for code clarity.

--- battle.py
+++ battle.py
@@ -1,4 +1,4 @@
-from arena import Arena
+"""Import all our dependencies"""
 import pygame
 from pygame import mixer
 from hero import Hero
@@ -7,57 +7,76 @@ from button import Button
 from weapon import Weapon
 from armour import Armour
 import random
-clock = pygame.time.Clock()
 
+"""Initialize our pygame"""
 pygame.init()
 mixer.init()
 
-# mixer.music.set_volume(1)
+"""All our pygame specific dependencies we need before runtime are here."""
 screen = pygame.display.set_mode([2000, 1250])
 background = pygame.image.load("Battle-Game-Images/FF4-arena.jpeg").convert_alpha()
 all_sprites = pygame.sprite.Group()
 attack_button = pygame.image.load("Battle-Game-Images/attack.jpeg").convert_alpha()
+clock = pygame.time.Clock()
 
-
+"""This is currently where we store the heroes. Should creat a file to hold these later if we want to change heroes."""
 cecil_harvey = Hero("Battle-Game-Images/Cecil.png", "Cecil Harvey", 3000)
-cecil_harvey.add_button(Button(100, 1100, attack_button, 3))
+cecil_harvey.add_attack_button(Button(100, 1100, attack_button, 3))
+cecil_harvey.add_special_button(Button(100, 1150, attack_button, 3))
 cecil_harvey.add_equipment(Weapon("Excalibur", "Sword", 500, 0))
 cecil_harvey.add_equipment(Armour("Mithril", 100))
 
 rosa_farrell = Hero("Battle-Game-Images/Rosa-copy.png", "Rosa Farrell", 1800)
-rosa_farrell.add_button(Button(500, 1100, attack_button, 3))
+rosa_farrell.add_attack_button(Button(500, 1100, attack_button, 3))
+rosa_farrell.add_special_button(Button(500, 1150, attack_button, 3))
 rosa_farrell.add_equipment(Weapon("Bow", "Bow", 200, 0))
 rosa_farrell.add_equipment(Armour("Robe", 50))
 
 edge_geraldin = Hero("Battle-Game-Images/Edge.png", "Edge Geraldin", 2000)
-edge_geraldin.add_button(Button(900, 1100, attack_button, 3))
+edge_geraldin.add_attack_button(Button(900, 1100, attack_button, 3))
+edge_geraldin.add_special_button(Button(900, 1150, attack_button, 3))
 edge_geraldin.add_equipment(Weapon("Katana", "Sword", 500, 0))
 edge_geraldin.add_equipment(Armour("Gi", 50))
 
 rydia_of_the_mist = Hero("Battle-Game-Images/Rydia-V1-copy.png", "Rydia of the Mist", 1800)
-rydia_of_the_mist.add_button(Button(1300, 1100, attack_button, 3))
+rydia_of_the_mist.add_attack_button(Button(1300, 1100, attack_button, 3))
+rydia_of_the_mist.add_special_button(Button(1300, 1150, attack_button, 3))
 rydia_of_the_mist.add_equipment(Weapon("Whip", "Whip", 200, 0))
 rydia_of_the_mist.add_equipment(Armour("Dress", 50))
 
 kain_highwind = Hero("Battle-Game-Images/Kain-V2-copy.png", "Kain Highwind", 2500)
-kain_highwind.add_button(Button(1700, 1100, attack_button, 3))
+kain_highwind.add_attack_button(Button(1700, 1100, attack_button, 3))
+kain_highwind.add_special_button(Button(1700, 1150, attack_button, 3))
 kain_highwind.add_equipment(Weapon("Dragon Lance", "Spear", 500, 0))
 kain_highwind.add_equipment(Armour("Dragon Armour", 100))
 
 hero = [cecil_harvey, rosa_farrell, edge_geraldin, rydia_of_the_mist, kain_highwind]
 
+"""Containing teams"""
 heroes = Team("Crystal Warriors")
 enemies = Team("Baddies")
 
+"""Auto fills our heroes on their team"""
 for dude in hero:
     heroes.add_hero(dude)
 
+
+"""This is to set the position of the hero cards. Could be better if this were lanes the team members got slotted into."""
 cecil_harvey.move(0, 600)
 rosa_farrell.move(400, 600)
 edge_geraldin.move(800, 600)
 rydia_of_the_mist.move(1200, 600)
 kain_highwind.move(1600, 600)
 
+"""This is the method run to change the song during runtime"""
+def play_music(song):
+    mixer.music.stop()
+    mixer.music.unload()
+    mixer.music.load(song)
+    mixer.music.play(-1)
+    return False
+
+"""All runtime specific variables to maintain operational flow."""
 boss_turn = 0
 turn = 0
 cecil_turn = 0
@@ -66,23 +85,31 @@ new_battle = True
 boss = False
 music = True
 experience = 0
+
+"""Start the game"""
 while running: 
-	# Looks at events 
+    """This is where we check for exiting the game."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+
+    """Here we set the background image."""
     screen.fill((255, 255, 255))
     screen.blit(background, [0, 0])
 
-
+    """This is our main Player combat loop, we check for button presses and apply the attack logic."""
     for hero in heroes.heroes:
-        if hero.button.draw(screen):
-            enemy = random.choice(enemies.heroes)
-            hero.attack(enemy)
-            if not enemy.is_alive():
-                enemies.remove_hero(enemy)
-                experience += 500
+        if hero.attack_button.draw(screen):
+            if len(enemies.heroes) == 0:
+                pass
+            else:
+                enemy = random.choice(enemies.heroes)
+                hero.attack(enemy)
+                if not enemy.is_alive():
+                    enemies.remove_hero(enemy)
+                    experience += 500
+
+    """This is to controle the enemy turn cycle to attacking once every 10seconds."""     
     turn += 1
     if turn == 10 * 60:
         for hero in heroes.heroes:
@@ -92,18 +119,16 @@ while running:
             enemy.attack(hit_hero)
             if not hit_hero.is_alive():
                 heroes.remove_hero(hit_hero)
-                
         turn = 0
-
+    """Here we render both teams characters onto the screen"""
     for entity in heroes.heroes:
         entity.render(screen)
     for entity in enemies.heroes:
         entity.render(screen)
 
+    """This is how we populate the enemy team between rounds."""
     if new_battle:
-        mixer.music.unload()
-        mixer.music.load("Battle-Game-Music/Fight.mp3")
-        mixer.music.play(-1)
+        music = play_music("Battle-Game-Music/Fight.mp3")
         number_enemies = random.randint(1, 5)
         while number_enemies > 0:
             enemies.add_hero(Hero("Battle-Game-Images/Goblin-copy.png", "Goblin", 500, 100))
@@ -114,15 +139,12 @@ while running:
             x += 400
         boss_turn += 1
         new_battle = False
+        music = True
 
-    # VIctory condition
+    """This is where we check victory conditions and what type of battle the player will face next."""
     if len(enemies.heroes) == 0:
         if music:
-            mixer.music.stop()
-            mixer.music.unload()
-            mixer.music.load("Battle-Game-Music/Fanfare.mp3")
-            mixer.music.play(-1)
-            music = False
+            music = play_music("Battle-Game-Music/Fanfare.mp3")
         new_fight_button = Button(1800, 300, attack_button, 4)
 
         if new_fight_button.draw(screen):
@@ -135,13 +157,10 @@ while running:
                 new_fight_button = ''
                 music = True
 
-
+    """Special settings for the boss round."""
     if boss:
         if music:
-            mixer.music.unload()
-            mixer.music.load("Battle-Game-Music/Boss.mp3")
-            mixer.music.play(-1)
-            music = False
+            music = play_music("Battle-Game-Music/Boss.mp3")
         enemies.add_hero(Hero("Battle-Game-Images/Behemoth-copy.png", "Behemoth", 50000, 500))
         boss = False
         music = True
@@ -150,6 +169,6 @@ while running:
 
 
 
-    # Update the display
+    """This is where we set the game refresh rate and tell the game to refresh."""
     pygame.display.flip()
     clock.tick(60)
